13f3c761af9376c26199a2e5a05896c34ac8f06c
shikha167 <pandey88shikha@gmail.com>
2024-01-31 20:03:36 -0500

    DaoModule

--- src/main/java/com/example/myapp/MyappApplication.java
+++ src/main/java/com/example/myapp/MyappApplication.java
@@ -1,13 +1,22 @@
 package com.example.myapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyappApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner-> {                                               //lambda expression
+			System.out.println("Hello World With Lambda expresssion");
+		};
 	}
 
 }
---
+++ src/main/java/com/example/myapp/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.example.myapp.dao;
+
+import com.example.myapp.entity.Student;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+
+}
